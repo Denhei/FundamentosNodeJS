661cfe44cd9ebcb04181d796864df40c50758ab3
Lucas Sergio Denhei Alexandre <denhei@hotmail.com>
2020-09-06 12:30:11 -0300

    Server aplicando conceitos de Model, Repositories, DTO's e Servicre para controle de depósitos e saques

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, current: Transaction) => {
+        switch (current.type) {
+          case 'income':
+            accumulator.income += current.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += current.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,18 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('Saldo insuficiente para esta operação');
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('The operation type must be income or outcome');
+    }
+
+    return this.transactionsRepository.create({ title, type, value });
   }
 }
 
